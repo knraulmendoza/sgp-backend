e1826c58eac6018ba1b87c5e34d5b4d23171525e
RmrGnzlz <rmrgnzlzlpz@gmail.com>
2019-11-17 17:19:38 -0500

    Arreglando Controlador Proyecto
    Consultar propuesta de todos los proyectos.

--- Dominio/Entities/Propuesta.cs
+++ Dominio/Entities/Propuesta.cs
@@ -10,7 +10,7 @@ namespace Dominio.Entities
         public DateTime FechaDePresentacion { get; set; }
 
         public DateTime FechaDeAprovacion { get; set; }
-
+        public long DocumentoId { get; set; }
         public Documento Documento { get; set; }
 
         public IList<Beneficiario> Beneficiarios { get; set; }
--- Dominio/Entities/Proyecto.cs
+++ Dominio/Entities/Proyecto.cs
@@ -6,39 +6,25 @@ namespace Dominio.Entities
 {
     public class Proyecto : BaseEntity
     {
+        public long PropuestaId { get; set; }
         public Propuesta Propuesta { get; set; }
-
         public ProyectoState ProyectoState { get; set; }
-
         public string Nombre { get; set; }
-
         public decimal PresupuestoAprobado { get; set; }
-
         public decimal PresupuestoEjecutado { get; set; }
-
         public List<ProyectoComunidad> ProyectosComunidads { get; set; }
-
         public DateTime FechaEjecucion { get; set; }
-
         public List<CertificadoDeDisponibilidadPresupuestal> CertificadosDeDisponibilidaPresupuestales { get; set; }
-
         /* Comentada por problemas de relación n a n */
         // public List<TransaccionBinaria> TransaccionesBinarias { get; set; }
-
         public List<TransaccionUnaria> TransaccionesUnarias { get; set; }
-
         public List<TransaccionBinaria> TransaccionesBinarias { get; set; }
-
         public DateTime FechaCierre { get; set; }
-
         public DateTime FechaDeCierrePrevista { get; set; }
-
+        public long ProgramaId { get; set; }
         public Programa Programa { get; set; }
-
         public List<Actividad> Actividades { get; set; }
-
         public IList<Beneficiario> Beneficiarios { get; set; }
-
         public Proyecto() { }
 
     }
